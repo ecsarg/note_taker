39fffb45638c9a481dff9da71a758e1f4ebcb63f
Ellie Sargent <elliecsargent@gmail.com>
2022-07-01 19:56:00 -0500

    need to complete and fix apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const app = express();
 const fs = require('fs');
-const path = require('path');
-
+const { v4: uuidv4 } = require('uuid');
+const app = require('./htmlRoutes');
 const savedNotes = require('./db/db.json');
 
-app.get
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    res.json(results)
+})
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, savedNotes)
+})
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('./db', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
