2f6d6987ed7ee13a7eccbca76f5a4d0d73d3eadd
Ellie Sargent <elliecsargent@gmail.com>
2022-07-03 20:56:06 -0500

    unable to get npm to start. added code to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,35 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const app = require('./htmlRoutes');
-const savedNotes = require('./db/db.json');
+const savedNotes = require('./Develop/db/db.json');
 
 app.get('/api/notes', (req, res) => {
-    let results = notes;
-    res.json(results)
-})
+    res.json(savedNotes.slice(1));
+});
+
+function createNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+    notesArray=[];
+
+    if (notesArray.length === 0)
+    notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
 
 app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, savedNotes)
-})
+    const newNote = createNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+
 
